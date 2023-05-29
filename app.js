e3d793f853f4962181964d3c785ac7d7eb00e471
Rimplstilckin <milos.protic@gmail.com>
2023-05-29 09:06:50 +0200

    Data fixed

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({
-        "humidity": 45, 
-        "temperature":  21,
-        "polution": 1230,
+        "humidity": 55, 
+        "temperature":  22.8,
+        "polution": 3410,
         "light": false,
         "warning": false
     }));
