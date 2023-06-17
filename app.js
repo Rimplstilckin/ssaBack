849c154c955b1f4cdae753e4156d69a60d6090dd
Rimplstilckin <milos.protic@gmail.com>
2023-06-18 00:14:25 +0200

    Server fixed

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const http = require('http');
 
-const server = http.createServer((req, res)=>{
-    // console.log(req.url, req.method, req.headers);
-    // res.setHeader('Content-Type', 'text/hmtl');
-    res.write('Test');
-    res.end();
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+        "humidity": 55, 
+        "temperature":  22.8,
+        "polution": 3410,
+        "light": false,
+        "warning": false
+    }));
 });
 
 server.listen(80);
