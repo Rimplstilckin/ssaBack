78ed5dcc64acd6a0ce72e01aa21efb474bc64515
Rimplstilckin <milos.protic@gmail.com>
2023-07-13 21:17:33 +0200

    Sever setup

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({
-        "humidity": 55, 
-        "temperature":  22.8,
-        "polution": 3410,
-        "light": false,
-        "warning": false
-    }));
+    const url =  req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write("<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Submit</button></form></body>");
+        res.write('</html>');
+        return res.end();
+    }
+    if(url==='/message' && method==='POST'){
+        const body = [];
+        req.on('data', (chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsebody = Buffer.concat(body).toString();
+            const message = parsebody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<ead><title>Stranica</title></head>');
+    res.write('<body><h1>Hello</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(8080);
