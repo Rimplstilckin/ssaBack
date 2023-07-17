604ec7b87aad5df013e568c2cb11aee68f0a6e99
Rimplstilckin <milos.protic@gmail.com>
2023-07-18 01:08:16 +0200

    Express added

--- app.js
+++ app.js
@@ -1,38 +1,9 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const url =  req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Enter message</title></head>');
-        res.write("<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Submit</button></form></body>");
-        res.write('</html>');
-        return res.end();
-    }
-    if(url==='/message' && method==='POST'){
-        const body = [];
-        req.on('data', (chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsebody = Buffer.concat(body).toString();
-            const message = parsebody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        })
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
+const express = require('express');
 
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<ead><title>Stranica</title></head>');
-    res.write('<body><h1>Hello</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+const app = express();
+
+const server = http.createServer(app);
 
 server.listen(8080);
